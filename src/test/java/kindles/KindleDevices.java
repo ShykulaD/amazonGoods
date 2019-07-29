c19cdd50eced1a54d0de16a614b90452b8856452
ShykulaD <de.shykula@gmail.com>
2019-07-29 15:51:41 +0300

    1

--- src/test/java/kindles/KindleDevices.java
+++ src/test/java/kindles/KindleDevices.java
@@ -1,8 +1,9 @@
-package kindle;
+package kindles;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import wrappers.BaseWrapper;
 
@@ -21,18 +22,21 @@ public class KindleDevices extends BaseWrapper {
             "https://www.amazon.com/dp/B07CXG6C9W?ref=ods_ucc_eink_pprwhite_nrc_ucc",
             "https://www.amazon.com/dp/B07F7TLZF4?ref=ods_ucc_eink_oasis_nrc_ucc"};
 
-
-    @Test(priority = 1, alwaysRun = true)
-    public void getUsualKindel() throws InterruptedException {
-        driver.get(kindleList[0]);
-
+    @BeforeTest
+    public void configureAmazon() throws InterruptedException {
+        driver.get("https://www.amazon.com/");
         WebDriverWait explicitWait = new WebDriverWait(driver, 10);
         explicitWait.until(ExpectedConditions.presenceOfElementLocated(changeUserLocation));
         driver.findElement(changeUserLocation).click();
-        driver.findElement(enterUSAZip).sendKeys("02140");
+        driver.findElement(enterUSAZip).sendKeys("10001");
         driver.findElement(applyUSAZip).click();
         driver.findElement(continueUSAZip).click();
-        Thread.sleep(3000);
+        Thread.sleep(4000);
+    }
+
+    @Test(priority = 1, alwaysRun = true)
+    public void getUsualKindel() {
+        driver.get(kindleList[0]);
 
         String parseUsualKindlePrice = driver.findElement(kindlePrice).getText();
         parseUsualKindlePrice = parseUsualKindlePrice.replaceAll("[$]", "").trim();
@@ -67,7 +71,6 @@ public class KindleDevices extends BaseWrapper {
         }
 
     }
-
 
     @Test(priority = 3, alwaysRun = true)
     public void getOasisWhitePrice() {
