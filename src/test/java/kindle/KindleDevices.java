5264d9cf6fe4d8b94bedde66d4ca06491b6093d6
ShykulaD <de.shykula@gmail.com>
2019-07-29 15:20:17 +0300

    Add new url/fix minor bugs

--- src/test/java/kindle/KindleDevices.java
+++ src/test/java/kindle/KindleDevices.java
@@ -9,7 +9,7 @@ import wrappers.BaseWrapper;
 /**
  * Created by Shykulad on 28/07/2019.
  */
-public class KindlePaperWhite extends BaseWrapper {
+public class KindleDevices extends BaseWrapper {
 
     private By changeUserLocation  = By.xpath("//input[@data-action-type='SELECT_LOCATION']");
     private By enterUSAZip  = By.xpath("//input[@class='GLUX_Full_Width a-declarative']");
@@ -17,32 +17,38 @@ public class KindlePaperWhite extends BaseWrapper {
     private By continueUSAZip  = By.xpath("//*[@class='a-popover-footer']//input");
     private By kindlePrice  = By.xpath("//span[@id='priceblock_ourprice' and @class='a-size-medium a-color-price priceBlockBuyingPriceString']");
 
-    @Test
-    public void gerPaperWhitePrice() throws InterruptedException {
-        driver.get("https://www.amazon.com/dp/B07DLPWYB7?ref_=nav_em_T1_0_4_11_1__k_ods_eink_jr&th=1");
+    String[] kindleList = new String[]{"https://www.amazon.com/dp/B07CXG6C9W?ref=ods_ucc_eink_pprwhite_nrc_ucc",
+            "",
+            "",
+            "",};
+
+    @Test(priority = 1, alwaysRun = true)
+    public void getPaperWhitePrice() throws InterruptedException {
+        driver.get(kindleList[0]);
 
            WebDriverWait explicitWait = new WebDriverWait(driver, 10);
            explicitWait.until(ExpectedConditions.presenceOfElementLocated(changeUserLocation));
             driver.findElement(changeUserLocation).click();
             driver.findElement(enterUSAZip).sendKeys("02140");
             driver.findElement(applyUSAZip).click();
-                Thread.sleep(2000);
             driver.findElement(continueUSAZip).click();
-                Thread.sleep(4000);
+            Thread.sleep(3000);
 
         String parseKindlePrice = driver.findElement(kindlePrice).getText();
         parseKindlePrice = parseKindlePrice.replaceAll("[$]", "").trim();
-        double kindlePriceInINT = Double.parseDouble(parseKindlePrice); // getting minutes from film hours
-        System.out.println("Kindle price is " + kindlePriceInINT);
+        double kindlePriceInINT = Double.parseDouble(parseKindlePrice);
+        System.out.println("KindlePaperWhite price is " + kindlePriceInINT);
 
-        if (Double.valueOf(kindlePriceInINT) == 89.99) {
-            System.out.println("Kindle price didn't change");
-        } else if (kindlePriceInINT >= 90) {
-            System.out.println("Kindle price is bigger than regular price");
-        } else if (kindlePriceInINT <= 89.98) {
-            System.out.println("Kindle price is lower than regular price! Time to buy");
+        if (Double.valueOf(kindlePriceInINT) == 129.99) {
+            System.out.println("KindlePaperWhite price didn't change");
+        } else if (kindlePriceInINT >= 130) {
+            System.out.println("KindlePaperWhite price is bigger than regular price");
+        } else if (kindlePriceInINT <= 129.98) {
+            System.out.println("KindlePaperWhite price is lower than regular price! Time to buy");
         }
 
     }
 
-}
+
+
+    }
