690057703ad1f18c9ee64500a1106b510868345f
ShykulaD <de.shykula@gmail.com>
2019-08-09 16:32:50 +0300

    1: 1 $

--- src/test/java/pages/UsualKindlePage.java
+++ src/test/java/pages/UsualKindlePage.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 /**
@@ -11,16 +12,20 @@ import org.testng.annotations.Test;
 
 public class UsualKindlePage {
 
+
+     String new1 = "[\"New\"]";
+    // ChromeDriver driver = new ChromeDriver();
     public WebDriver driver;
    private String[] kindleList = new String[]{"https://www.amazon.com/dp/B07DLPWYB7?ref=ods_ucc_eink_kindle_nrc_ucc",
             "https://www.amazon.com/dp/B07CXG6C9W?ref=ods_ucc_eink_pprwhite_nrc_ucc",
             "https://www.amazon.com/dp/B07F7TLZF4?ref=ods_ucc_eink_oasis_nrc_ucc"};
 
-    private By kindlePrice = By.xpath("//span[@id='priceblock_ourprice' and @class='a-size-medium a-color-price priceBlockBuyingPriceString']");
+    private By kindlePrice = By.xpath("//span[@id='priceblock_ourprice' and " +
+            "@class='a-size-medium a-color-price priceBlockBuyingPriceString']");
 
     @Test
     public void getUsualKindel() {
-        driver.get(kindleList[0]);
+        driver.get("https://www.amazon.com/dp/B07DLPWYB7?ref=ods_ucc_eink_kindle_nrc_ucc");
 
         String parseUsualKindlePrice = driver.findElement(kindlePrice).getText();
         parseUsualKindlePrice = parseUsualKindlePrice.replaceAll("[$]", "").trim();
